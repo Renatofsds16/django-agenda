63f9e7d18a0cf531d86eac6f11d043d6a76c3395
Renato Firmino santos da silva <renatofss16@gmail.com>
2025-04-05 21:37:06 -0300

    finalizando o site agenda

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,7 @@ from .models import Contact
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import password_validation
 
 
 6
@@ -10,11 +11,14 @@ class ContactForm(forms.ModelForm):
 
 
     picture = forms.ImageField(
+        required=False,
         widget=forms.FileInput(
             attrs={
                 'accept': 'image\*',
             }
+            
         )
+        
     )
     class Meta:
         model = Contact
@@ -65,4 +69,84 @@ class RegisterForm(UserCreationForm):
         if User.objects.filter(email=email).exists():
             self.add_error('email',ValidationError('email ja existe'))
         return email
+
+
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=50,
+        required=True,
+        error_messages= {
+            'min_length': 'por favor digite 2 letras ou mais'
+        }
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=50,
+        required=True,
+        error_messages= {
+            'min_length': 'por favor digite 2 letras ou mais'
+        }
+    )
+
+    password1 = forms.CharField(
+        label='Password',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete':'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False
+    )
+
+    password2 = forms.CharField(
+        label='Password',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete':'new-password'}),
+        help_text='as senha deven ser iguais',
+        required=False,
+    )
+    class Meta:
+        model = User
+        fields = 'first_name','last_name','email','username',
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error('email',ValidationError('email ja existe'))
+        return email
+    
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as error:
+                self.add_error('password1',ValidationError(error))
+        return password1
+    
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error('password2',ValidationError('as senha nao são iguais'))
+
+        return super().clean()
+    
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user: User = super().save(commit=False)
+        password = cleaned_data.get('password1')
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
         
+
+        return user
+
+        
+
--- contact/views.py
+++ contact/views.py
@@ -3,11 +3,12 @@ from .models import Contact
 from django.http import Http404
 from django.db.models import Q
 from django.core.paginator import Paginator
-from .forms import ContactForm, RegisterForm
+from .forms import ContactForm, RegisterForm, RegisterUpdateForm
 from django.urls import reverse
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -51,23 +52,25 @@ def search(request):
     context = {'page_obj':page_obj,'site_title':'contatos - '}
     return render(request,'contact\index.html',context)
 
-
+@login_required(login_url='login')
 def create(request):
     form_action = reverse('create')
     if request.method == 'POST':
         form = ContactForm(request.POST,request.FILES)
         context = {'form':form,'form_action':form_action}
         if form.is_valid():
-            contact = form.save()
-            return redirect('update',contact_id=contact.id)
+            contact: Contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
+            return redirect('contacts')
         return render(request,'contact\create.html',context)
     
     context = {'form':ContactForm(),'form_action':form_action}
     return render(request,'contact\create.html',context)
     
-
+@login_required(login_url='login')
 def update(request,contact_id):
-    contact = get_object_or_404(Contact,id=contact_id,show=True)
+    contact = get_object_or_404(Contact,id=contact_id,show=True,owner=request.user)
     form_action = reverse('update',args=(contact_id,))
     
     if request.method == 'POST':
@@ -82,9 +85,9 @@ def update(request,contact_id):
     context = {'form':ContactForm(instance=contact),'form_action':form_action}
     return render(request,'contact\create.html',context)
 
-
+@login_required(login_url='login')
 def delete(request,contact_id):
-    contact = get_object_or_404(Contact,id=contact_id,show=True)
+    contact = get_object_or_404(Contact,id=contact_id,show=True,owner=request.user)
     confirmation = request.POST.get('confirmation','no')
     if confirmation == 'yes':
         contact.delete()
@@ -100,6 +103,7 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            redirect('contacts')
     return render(request,'contact/register.html',{'form':form})
 
 def login_view(request):
@@ -115,7 +119,17 @@ def login_view(request):
         else:
             messages.error(request,'não foi posivel fazer login')
     return render(request,'contact/login.html',{'form':form})
-
+@login_required(login_url='login')
 def logout_view(request):
     auth.logout(request)
     return redirect('login')
+@login_required(login_url='login')
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+    if request.method == 'POST':
+        form = RegisterUpdateForm(instance=request.user,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contacts')
+    return render(request,'contact/register.html',{'form':form})
+    
